bd31ec5359c9392969c6b11951e281a84597277a
Oskar The Stupid Valentine God <spelfanta2@gmail.com>
2020-03-23 11:33:19 +0100

    Update mesh.h
    clean up

--- src/engine/scene/mesh.h
+++ src/engine/scene/mesh.h
@@ -13,26 +13,15 @@ namespace me {
 
   struct mesh {
     std::string identifier;
-    unsigned int VAO, VBO, EBO;
     std::vector<vertex> vertices;
     std::vector<unsigned int> indices;
-    std::vector<me::vec3f> positions;
-    std::vector<me::vec3f> normals;
-    std::vector<me::vec2f> texCoords;
-    me::material* material;
+    me::material material;
 
-    mesh(std::string identifier, unsigned int VAO, unsigned int VBO, unsigned int EBO, std::vector<vertex> vertices,std::vector<unsigned int> indices,
-    std::vector<me::vec3f> positions, std::vector<me::vec3f> normals, std::vector<me::vec2f> texCoords, me::material* material)
+    mesh(std::string identifier, std::vector<vertex> vertices, std::vector<unsigned int> indices, me::material material)
     {
       this->identifier = identifier;
-      this->VAO = VAO;
-      this->VBO = VBO;
-      this->EBO = EBO;
       this->vertices = vertices;
       this->indices = indices;
-      this->positions = positions;
-      this->normals = normals;
-      this->texCoords = texCoords;
       this->material = material;
     }
 
@@ -40,14 +29,15 @@ namespace me {
 
     ~mesh()
     {
-
+      vertices.clear();
+      indices.clear();
     }
 
   };
 
   struct mesh_item : item {
-    me::mesh* mesh;
-    mesh_item(std::string identifier, me::vec3d* position, me::vec3d* rotation, me::vec3d* scale, me::mesh* mesh) :
+    unsigned int mesh;
+    mesh_item(std::string identifier, me::vec3d position, me::vec3d rotation, me::vec3d scale, unsigned int mesh) :
     item(ME_ITEM_TYPE_MESH, identifier, position, rotation, scale)
     {
       this->mesh = mesh;
